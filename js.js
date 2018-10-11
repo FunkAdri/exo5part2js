090c1fd9ebcc9221c478032c5df24695b1c15016
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-11 09:41:39 +0200

    fini

---
+++ js.js
@@ -0,0 +1,24 @@
+//Ajout d'un évènement lorsque la souris passe au dessus de l'image
+image1.addEventListener("mouseover",function(){
+//Paramètre de la fonction image1.src = prendre l'id utiliser dans le html
+  image1.src="assets/image1_2.jpg";
+});
+
+//On recommence ?
+image2.addEventListener("mouseover",function(){
+  image2.src="assets/image2_2.jpg";
+});
+
+// Image 3
+image3.addEventListener("mouseover",function(){
+  image3.src="assets/image3_2.jpg";
+});
+
+// Image 4
+image4.addEventListener("mouseover",function(){
+  image4.src="assets/image4_2.jpg";
+});
+
+// Image5
+image5.addEventListener("mouseover",function(){
+  image5.src="assets/image5_2.jpg";
